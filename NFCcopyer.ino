3c18e4f91273962d5cc33b56eaeee9cfc9f281f8
Goldbeibei <102613276+Goldbeibei@users.noreply.github.com>
2024-03-11 15:13:21 +0800

    更新 NFCcopyer.ino

--- NFCcopyer.ino
+++ NFCcopyer.ino
@@ -1,58 +1,37 @@
 #include <SPI.h>
 #include <MFRC522.h>
 
-/*----------------------
-RFID-RC522     Arduino
-------------------------
-3.3V           3.3V
-GND            GND
-RST            D9
-SDA (SS)       D10
-SCK            D13
-MOSI           D11
-MISO           D12
-IRQ            -
-----------------------*/
+#define RST_PIN         9           // Configurable, see typical pin layout above
+#define SS_PIN          10          // Configurable, see typical pin layout above
 
-#define RST_PIN         9          
-#define SS_PIN          10  //就是模組上的SDA接腳
-
-
-MFRC522 mfrc522;   // 建立MFRC522實體
+MFRC522 mfrc522(SS_PIN, RST_PIN);  // Create MFRC522 instance
 
 void setup() {
-
-  Serial.begin(115200); 
-
-  SPI.begin();        // 初始化SPI介面
-
-  mfrc522.PCD_Init(SS_PIN, RST_PIN); // 初始化MFRC522卡
-  Serial.print(F("Reader "));
-  Serial.print(F(": "));
-  mfrc522.PCD_DumpVersionToSerial(); // 顯示讀卡設備的版本
-  
+  Serial.begin(9600);     // Initialize serial communications with the PC
+  while (!Serial);        // Do nothing if no serial port is opened (added for Arduinos based on ATMEGA32U4)
+  SPI.begin();            // Init SPI bus
+  mfrc522.PCD_Init();     // Init MFRC522 card
+  Serial.println("Scan your RFID/NFC tag...");
 }
-
 
 void loop() {
+  // Look for new cards
+  if (!mfrc522.PICC_IsNewCardPresent()) {
+    return;
+  }
 
-  // 檢查是不是一張新的卡
-  if (mfrc522.PICC_IsNewCardPresent() && mfrc522.PICC_ReadCardSerial()) {
-      // 顯示卡片內容
-      Serial.print(F("Card UID:"));
-      dump_byte_array(mfrc522.uid.uidByte, mfrc522.uid.size); // 顯示卡片的UID
-      Serial.println();
-      Serial.print(F("PICC type: "));
-      MFRC522::PICC_Type piccType = mfrc522.PICC_GetType(mfrc522.uid.sak);
-      Serial.println(mfrc522.PICC_GetTypeName(piccType));  //顯示卡片的類型
+  // Select one of the cards
+  if (!mfrc522.PICC_ReadCardSerial()) {
+    return;
+  }
 
-      mfrc522.PICC_HaltA();  // 卡片進入停止模式
-    }
+  // Show UID on serial monitor
+  Serial.print("Card UID:");
+  dump_byte_array(mfrc522.uid.uidByte, mfrc522.uid.size);
+  Serial.println();
+  mfrc522.PICC_HaltA();
 }
 
-/**
- * 這個副程式把讀取到的UID，用16進位顯示出來
- */
 void dump_byte_array(byte *buffer, byte bufferSize) {
   for (byte i = 0; i < bufferSize; i++) {
     Serial.print(buffer[i] < 0x10 ? " 0" : " ");
